712d5398e2b09f781c5d4a34c76cbd7ecc35745a
kramwalkis <julius.kramwalkis@gmail.com>
2021-03-12 13:49:26 +0200

    rendered board

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('battle ship begins');
+const container = document.getElementById('container')
+const arrSquareIds = []
+
+
+function renderSquares() {
+    for (let x=0; x<10; x++) {
+        for (let y=0; y<10; y++) {
+            let slot = document.createElement('div')
+            slot.setAttribute('id', `x:${x} y:${y}`)
+            arrSquareIds.push(`x:${x} y:${y}`)
+            container.appendChild(slot)
+        }
+    }
+}
+
+renderSquares() 
+
