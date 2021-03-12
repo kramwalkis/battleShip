028ee6bd4fd49b7d94172fe4a5aaa3e82431517c
kramwalkis <julius.kramwalkis@gmail.com>
2021-03-12 16:02:03 +0200

    function to calculate positions refactored so can be used for all size ships

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
 const container = document.getElementById("container");
 const arrSquareIds = [];
-let arrayOfShips4 = []
+let arrayOfShips4 = [];
+let arrayOfShips3 = [];
+let arrayOfShips2 = [];
+let arrayOfShips1 = []
 
-function renderSquares() {
-  let number = 0;
+function renderSquares() {  
   for (let x = 0; x < 100; x++) {
     let slot = document.createElement("div");
     slot.setAttribute("id", x);
-    slot.innerText = number;
-    number++;
-    arrSquareIds.push(x);
+    slot.innerText = x;    
+    arrSquareIds.push(x.toString());
     container.appendChild(slot);
   }
 }
 
 renderSquares();
 
-function allPlacesFor4Piece(arr, times) {
+function renderPossiblePositions(arr, times) {
   let verticalArr = [];
   let horizontalArr = [];
-  arrSquareIds.map((item) => {      
-    let positionHorizontal = [item, item + 1, item + 2, item + 3];
-    let positionVertical = [item, item + 10, item + 20, item + 30];
-    let number = item.toString();
-    if (
-      number[number.length - 1] !== "7" &&
-      number[number.length - 1] !== "8" &&
-      number[number.length - 1] !== "9"
-    ) {
+  arrSquareIds.map((item) => {
+    let positionHorizontal = [];
+    let positionVertical = [];
+    for (let x = 0; x < times; x++) {
+      positionHorizontal.push((Number(item) + x).toString());
+      positionVertical.push((Number(item) + x * 10).toString());
+    }    
+    let horizontalConditions = [
+      item[item.length - 1] !== "7",
+      item[item.length - 1] !== "8",
+      item[item.length - 1] !== "9",
+    ];
+    let verticalConditions = [
+      item[0] !== "7",
+      item[0] !== "8",
+      item[0] !== "9",
+    ];
+    while (times - horizontalConditions.length !== 1) {
+      horizontalConditions.shift();
+    }
+    while (times - verticalConditions.length !== 1) {
+      verticalConditions.shift();
+    }
+    if (horizontalConditions.every((e) => e === true)) {
       horizontalArr.push(positionHorizontal);
     }
-    if (number.length === 1) {
-        verticalArr.push(positionVertical);
-    } else if (number[0] !== "7" && number[0] !== "8" && number[0] !== "9") {
-        verticalArr.push(positionVertical);
-    }    
+    if (item.length === 1) {
+      verticalArr.push(positionVertical);
+    } else if (verticalConditions.every((e) => e === true)) {
+      verticalArr.push(positionVertical);
+    }
   });
-  arr = [...horizontalArr, ...verticalArr]
-  return arr
-    
+  arr = [...horizontalArr, ...verticalArr];
+  return arr;
 }
-arrayOfShips4 = allPlacesFor4Piece(arrayOfShips4, 4);
+arrayOfShips4 = renderPossiblePositions(arrayOfShips4, 4);
+arrayOfShips3 = renderPossiblePositions(arrayOfShips3, 3);
+arrayOfShips2 = renderPossiblePositions(arrayOfShips2, 2);
+arrayOfShips1 = [...arrSquareIds]
 
-console.log(arrayOfShips4);
+
+
