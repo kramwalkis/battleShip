b797f75bf09efd3e84402091a8cb7ce581d37061
kramwalkis <julius.kramwalkis@gmail.com>
2021-03-14 22:19:42 +0200

    ships are being put randomly on board. testing needed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.getElementById("container");
-const arrSquareIds = [];
+let arrSquareIds = [];
 let arrayOfShips4 = [];
 let arrayOfShips3 = [];
 let arrayOfShips2 = [];
 let arrayOfShips1 = [];
-let randomSelectedShip4 = []
+let randomSelectedShip4 = [];
+let randomSelectedShip3 = [];
+let randomSelectedShip2 = [];
+let selectedShips = []
 
 function renderSquares() {
   for (let x = 0; x < 100; x++) {
@@ -99,10 +102,113 @@ function findNeighbors(num) {
 }
 
 function randomSelectFromArr(arr) {
-    return arr[Math.floor(Math.random() * arr.length)]
+  return arr[Math.floor(Math.random() * arr.length)];  
+}
+
+function updateEmptySlots(ship, arr) {
+  let arrWithNeighbors = [];
+  ship.map((item) => {
+    arrWithNeighbors = [...arrWithNeighbors, ...findNeighbors(item)];
+  });
+  arrWithNeighbors = [...new Set(arrWithNeighbors)];
+  return arr.filter((item) => !arrWithNeighbors.includes(item));
+}
+
+function updateArrOfShips(shipArray, arr) {
+  let array = shipArray.filter((item) => !checkIfIncludes(item, arr));
+  return array;
+}
+
+function checkIfIncludes(arr, checker) {
+  let returnArray = arr.filter((item) => checker.includes(item));
+  return returnArray.length !== arr.length ? true : false;
 }
 
 randomSelectedShip4 = randomSelectFromArr(arrayOfShips4);
+selectedShips.push(randomSelectedShip4)
+
+arrSquareIds = updateEmptySlots(randomSelectedShip4, arrSquareIds);
+arrayOfShips3 = updateArrOfShips(arrayOfShips3, arrSquareIds);
+randomSelectedShip3 = randomSelectFromArr(arrayOfShips3);
+selectedShips.push(randomSelectedShip3)
+
+
+arrSquareIds = updateEmptySlots(randomSelectedShip3, arrSquareIds);
+arrayOfShips3 = updateArrOfShips(arrayOfShips3, arrSquareIds);
+randomSelectedShip3 = randomSelectFromArr(arrayOfShips3);
+selectedShips.push(randomSelectedShip3)
+
+arrSquareIds = updateEmptySlots(randomSelectedShip3, arrSquareIds);
+arrayOfShips2 = updateArrOfShips(arrayOfShips2, arrSquareIds);
+randomSelectedShip2 = randomSelectFromArr(arrayOfShips2);
+selectedShips.push(randomSelectedShip2)
+
+arrSquareIds = updateEmptySlots(randomSelectedShip2, arrSquareIds);
+arrayOfShips2 = updateArrOfShips(arrayOfShips2, arrSquareIds);
+randomSelectedShip2 = randomSelectFromArr(arrayOfShips2);
+selectedShips.push(randomSelectedShip2)
+
+arrSquareIds = updateEmptySlots(randomSelectedShip2, arrSquareIds);
+arrayOfShips2 = updateArrOfShips(arrayOfShips2, arrSquareIds);
+randomSelectedShip2 = randomSelectFromArr(arrayOfShips2);
+selectedShips.push(randomSelectedShip2)
+
+arrSquareIds = updateEmptySlots(randomSelectedShip2, arrSquareIds);
+console.log(arrSquareIds);
+selectedShips.push([randomSelectFromArr(arrSquareIds)])
+arrSquareIds = updateEmptySlots(selectedShips[selectedShips.length-1], arrSquareIds);
+console.log(arrSquareIds);
+selectedShips.push([randomSelectFromArr(arrSquareIds)])
+arrSquareIds = updateEmptySlots(selectedShips[selectedShips.length-1], arrSquareIds);
+console.log(arrSquareIds);
+selectedShips.push([randomSelectFromArr(arrSquareIds)])
+arrSquareIds = updateEmptySlots(selectedShips[selectedShips.length-1], arrSquareIds);
+console.log(arrSquareIds);
+selectedShips.push([randomSelectFromArr(arrSquareIds)])
+
+
+function spreadSlotsToColor(arr) {
+    let returnArray = []
+    arr.map(item => {
+    returnArray = [...returnArray, ...item]
+    })
+    return returnArray
+}
+
+function color(arr, slots) {
+  let array = Array.from(arr)  
+  array.map(item => {
+     let pixel = document.getElementById(item.id)
+     console.log(pixel);
+     if (slots.includes(Number(pixel.id))) {
+         console.log('hello');
+         pixel.classList.add('red')
+     } 
+  })
+}
+
+let slotsToColor = spreadSlotsToColor(selectedShips)
+console.log(slotsToColor);
+color(container.children, slotsToColor)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(selectedShips);
