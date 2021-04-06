2f6255856f75a65eaefc680090d40c1377ee30ba
kramwalkis <julius.kramwalkis@gmail.com>
2021-04-06 08:03:01 +0300

    refactored rendering functions

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const start = document.getElementById("start");
 
 let points = 120;
 let counter = 0;
+let pcCounter = 0;
 let arrSquareIds = [];
 let arrayOfShips4 = [];
 let arrayOfShips3 = [];
@@ -35,168 +36,171 @@ function reRender() {
   arrOfHitShips = [...selectedShips];
 }
 
-function renderSquares() {
-  for (let x = 0; x < 100; x++) {
-    let slot = document.createElement("div");
-    slot.classList.add("teal");
-    slot.setAttribute("id", x);
-    slot.addEventListener("click", shotTarget);
-    // slot.innerText = x;
-    arrSquareIds.push(x);
-    container.appendChild(slot);
-  }
-}
-
-function shotTarget(e) {
-  let id = e.target.id;
-  color(id, selectedShips);
-}
-
-function renderShips(arrOfId, arr2, arr3, arr4) {
-  let selected = [];
-  let returnArray = [];
-
-  selected = randomSelectFromArr(arr4);
-  returnArray.push(selected);
-
-  arrOfId = updateEmptySlots(selected, arrOfId);
-  arr3 = updateArrOfShips(arr3, arrOfId);
-  selected = randomSelectFromArr(arr3);
-  returnArray.push(selected);
-
-  arrOfId = updateEmptySlots(selected, arrOfId);
-  arr3 = updateArrOfShips(arr3, arrOfId);
-  selected = randomSelectFromArr(arr3);
-  returnArray.push(selected);
-
-  arrOfId = updateEmptySlots(selected, arrOfId);
-  arr2 = updateArrOfShips(arr2, arrOfId);
-  selected = randomSelectFromArr(arr2);
-  returnArray.push(selected);
-
-  arrOfId = updateEmptySlots(selected, arrOfId);
-  arr2 = updateArrOfShips(arr2, arrOfId);
-  selected = randomSelectFromArr(arr2);
-  returnArray.push(selected);
-
-  arrOfId = updateEmptySlots(selected, arrOfId);
-  arr2 = updateArrOfShips(arr2, arrOfId);
-  selected = randomSelectFromArr(arr2);
-  returnArray.push(selected);
-
-  arrOfId = updateEmptySlots(selected, arrOfId);
-  returnArray.push([randomSelectFromArr(arrOfId)]);
-  arrOfId = updateEmptySlots(returnArray[returnArray.length - 1], arrOfId);
-
-  returnArray.push([randomSelectFromArr(arrOfId)]);
-  arrOfId = updateEmptySlots(returnArray[returnArray.length - 1], arrOfId);
-
-  returnArray.push([randomSelectFromArr(arrOfId)]);
-  arrOfId = updateEmptySlots(returnArray[returnArray.length - 1], arrOfId);
-  returnArray.push([randomSelectFromArr(arrOfId)]);
-
-  return returnArray;
-}
-
-function renderPossiblePositions(arr, times) {
-  let verticalArr = [];
-  let horizontalArr = [];
-  arrSquareIds.map((item) => {
-    let positionHorizontal = [];
-    let positionVertical = [];
-    for (let x = 0; x < times; x++) {
-      positionHorizontal.push(item + x);
-      positionVertical.push(item + x * 10);
+const renderFunc = {
+  renderSquares: () => {
+    for (let x = 0; x < 100; x++) {
+      let slot = document.createElement("div");
+      slot.classList.add("teal");
+      slot.setAttribute("id", x);
+      slot.addEventListener("click", shotTarget);
+      arrSquareIds.push(x);
+      container.appendChild(slot);
     }
-    let horizontalConditions = [
-      item.toString()[item.toString().length - 1] !== "7",
-      item.toString()[item.toString().length - 1] !== "8",
-      item.toString()[item.toString().length - 1] !== "9",
+  },
+  renderShips: (arrOfId, arr2, arr3, arr4) => {
+    let selected = [];
+    let returnArray = [];
+
+    selected = renderFunc.randomSelectFromArr(arr4);
+    returnArray.push(selected);
+
+    arrOfId = renderFunc.updateEmptySlots(selected, arrOfId);
+    arr3 = renderFunc.updateArrOfShips(arr3, arrOfId);
+    selected = renderFunc.randomSelectFromArr(arr3);
+    returnArray.push(selected);
+
+    arrOfId = renderFunc.updateEmptySlots(selected, arrOfId);
+    arr3 = renderFunc.updateArrOfShips(arr3, arrOfId);
+    selected = renderFunc.randomSelectFromArr(arr3);
+    returnArray.push(selected);
+
+    arrOfId = renderFunc.updateEmptySlots(selected, arrOfId);
+    arr2 = renderFunc.updateArrOfShips(arr2, arrOfId);
+    selected = renderFunc.randomSelectFromArr(arr2);
+    returnArray.push(selected);
+
+    arrOfId = renderFunc.updateEmptySlots(selected, arrOfId);
+    arr2 = renderFunc.updateArrOfShips(arr2, arrOfId);
+    selected = renderFunc.randomSelectFromArr(arr2);
+    returnArray.push(selected);
+
+    arrOfId = renderFunc.updateEmptySlots(selected, arrOfId);
+    arr2 = renderFunc.updateArrOfShips(arr2, arrOfId);
+    selected = renderFunc.randomSelectFromArr(arr2);
+    returnArray.push(selected);
+
+    arrOfId = renderFunc.updateEmptySlots(selected, arrOfId);
+    returnArray.push([renderFunc.randomSelectFromArr(arrOfId)]);
+    arrOfId = renderFunc.updateEmptySlots(
+      returnArray[returnArray.length - 1],
+      arrOfId
+    );
+
+    returnArray.push([renderFunc.randomSelectFromArr(arrOfId)]);
+    arrOfId = renderFunc.updateEmptySlots(
+      returnArray[returnArray.length - 1],
+      arrOfId
+    );
+
+    returnArray.push([renderFunc.randomSelectFromArr(arrOfId)]);
+    arrOfId = renderFunc.updateEmptySlots(
+      returnArray[returnArray.length - 1],
+      arrOfId
+    );
+    returnArray.push([renderFunc.randomSelectFromArr(arrOfId)]);
+
+    return returnArray;
+  },
+  renderPossiblePositions: (arr, times) => {
+    let verticalArr = [];
+    let horizontalArr = [];
+    arrSquareIds.map((item) => {
+      let positionHorizontal = [];
+      let positionVertical = [];
+      for (let x = 0; x < times; x++) {
+        positionHorizontal.push(item + x);
+        positionVertical.push(item + x * 10);
+      }
+      let horizontalConditions = [
+        item.toString()[item.toString().length - 1] !== "7",
+        item.toString()[item.toString().length - 1] !== "8",
+        item.toString()[item.toString().length - 1] !== "9",
+      ];
+      let verticalConditions = [
+        item.toString()[0] !== "7",
+        item.toString()[0] !== "8",
+        item.toString()[0] !== "9",
+      ];
+      while (times - horizontalConditions.length !== 1) {
+        horizontalConditions.shift();
+      }
+      while (times - verticalConditions.length !== 1) {
+        verticalConditions.shift();
+      }
+      if (horizontalConditions.every((e) => e === true)) {
+        horizontalArr.push(positionHorizontal);
+      }
+      if (item.toString().length === 1) {
+        verticalArr.push(positionVertical);
+      } else if (verticalConditions.every((e) => e === true)) {
+        verticalArr.push(positionVertical);
+      }
+    });
+    arr = [...horizontalArr, ...verticalArr];
+    return arr;
+  },
+  findNeighbors: (num) => {
+    if (num === 0) {
+      return [num, 1, 10, 11];
+    }
+    if (num === 9) {
+      return [num, 8, 18, 19];
+    }
+    if (num === 90) {
+      return [num, 80, 81, 91];
+    }
+    if (num === 99) {
+      return [num, 88, 89, 98];
+    }
+    if (num >= 1 && num <= 8) {
+      return [num, num - 1, num + 1, num + 9, num + 10, num + 11];
+    }
+    if (num >= 91 && num <= 98) {
+      return [num, num - 1, num - 11, num - 10, num - 9, num + 1];
+    }
+    if (num !== 90 && num !== 0 && num.toString()[1] === "0") {
+      return [num, num - 10, num - 9, num + 1, num + 11, num + 10];
+    }
+    if (num !== 9 && num !== 99 && num.toString()[1] === "9") {
+      return [num, num - 10, num - 11, num - 1, num + 9, num + 10];
+    }
+    return [
+      num,
+      num - 11,
+      num - 10,
+      num - 9,
+      num + 1,
+      num + 11,
+      num + 10,
+      num + 9,
+      num - 1,
     ];
-    let verticalConditions = [
-      item.toString()[0] !== "7",
-      item.toString()[0] !== "8",
-      item.toString()[0] !== "9",
-    ];
-    while (times - horizontalConditions.length !== 1) {
-      horizontalConditions.shift();
-    }
-    while (times - verticalConditions.length !== 1) {
-      verticalConditions.shift();
-    }
-    if (horizontalConditions.every((e) => e === true)) {
-      horizontalArr.push(positionHorizontal);
-    }
-    if (item.toString().length === 1) {
-      verticalArr.push(positionVertical);
-    } else if (verticalConditions.every((e) => e === true)) {
-      verticalArr.push(positionVertical);
-    }
-  });
-  arr = [...horizontalArr, ...verticalArr];
-  return arr;
-}
-
-function findNeighbors(num) {
-  if (num === 0) {
-    return [num, 1, 10, 11];
-  }
-  if (num === 9) {
-    return [num, 8, 18, 19];
-  }
-  if (num === 90) {
-    return [num, 80, 81, 91];
-  }
-  if (num === 99) {
-    return [num, 88, 89, 98];
-  }
-  if (num >= 1 && num <= 8) {
-    return [num, num - 1, num + 1, num + 9, num + 10, num + 11];
-  }
-  if (num >= 91 && num <= 98) {
-    return [num, num - 1, num - 11, num - 10, num - 9, num + 1];
-  }
-  if (num !== 90 && num !== 0 && num.toString()[1] === "0") {
-    return [num, num - 10, num - 9, num + 1, num + 11, num + 10];
-  }
-  if (num !== 9 && num !== 99 && num.toString()[1] === "9") {
-    return [num, num - 10, num - 11, num - 1, num + 9, num + 10];
-  }
-  return [
-    num,
-    num - 11,
-    num - 10,
-    num - 9,
-    num + 1,
-    num + 11,
-    num + 10,
-    num + 9,
-    num - 1,
-  ];
-}
-
-function randomSelectFromArr(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-
-function updateEmptySlots(ship, arr) {
-  let arrWithNeighbors = [];
-  ship.map((item) => {
-    arrWithNeighbors = [...arrWithNeighbors, ...findNeighbors(item)];
-  });
-  arrWithNeighbors = [...new Set(arrWithNeighbors)];
-  return arr.filter((item) => !arrWithNeighbors.includes(item));
-}
-
-function updateArrOfShips(shipArray, arr) {
-  let array = shipArray.filter((item) => !checkIfIncludes(item, arr));
-  return array;
-}
-
-function checkIfIncludes(arr, checker) {
-  let returnArray = arr.filter((item) => checker.includes(item));
-  return returnArray.length !== arr.length ? true : false;
-}
+  },
+  randomSelectFromArr: (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  },
+  updateEmptySlots: (ship, arr) => {
+    let arrWithNeighbors = [];
+    ship.map((item) => {
+      arrWithNeighbors = [
+        ...arrWithNeighbors,
+        ...renderFunc.findNeighbors(item),
+      ];
+    });
+    arrWithNeighbors = [...new Set(arrWithNeighbors)];
+    return arr.filter((item) => !arrWithNeighbors.includes(item));
+  },
+  updateArrOfShips: (shipArray, arr) => {
+    let array = shipArray.filter(
+      (item) => !renderFunc.checkIfIncludes(item, arr)
+    );
+    return array;
+  },
+  checkIfIncludes: (arr, checker) => {
+    let returnArray = arr.filter((item) => checker.includes(item));
+    return returnArray.length !== arr.length ? true : false;
+  },
+};
 
 function color(id, arr) {
   let trigger = false;
@@ -251,11 +255,16 @@ function missedShot(id, arr) {
   score.innerText = `Score: ${points}`;
 }
 
-renderSquares();
-arrayOfShips4 = renderPossiblePositions(arrayOfShips4, 4);
-arrayOfShips3 = renderPossiblePositions(arrayOfShips3, 3);
-arrayOfShips2 = renderPossiblePositions(arrayOfShips2, 2);
-selectedShips = renderShips(
+function shotTarget(e) {
+  let id = e.target.id;
+  color(id, selectedShips);
+}
+
+renderFunc.renderSquares();
+arrayOfShips4 = renderFunc.renderPossiblePositions(arrayOfShips4, 4);
+arrayOfShips3 = renderFunc.renderPossiblePositions(arrayOfShips3, 3);
+arrayOfShips2 = renderFunc.renderPossiblePositions(arrayOfShips2, 2);
+selectedShips = renderFunc.renderShips(
   arrSquareIds,
   arrayOfShips2,
   arrayOfShips3,
@@ -266,54 +275,116 @@ arrOfHitShips = [...selectedShips];
 ///////////////////////
 
 /// Try to make AI
-let allPositions = [];
-let positionCounter = {};
+// let allPositions = [];
+// let positionCounter = {};
 
-function allShipsCombine() {
-  for (let x = 0; x <= 99; x++) {
-    arrayOfShips1.push([x]);
-    let id = x.toString();
-    // console.log(id);
-    positionCounter = {
-      ...positionCounter,
-      [id]: 0,
-    };
-  }
-  allPositions = [
-    ...arrayOfShips1,
-    ...arrayOfShips2,
-    ...arrayOfShips3,
-    ...arrayOfShips4,
-  ];
-}
+// function allShipsCombine() {
+//   for (let x = 0; x <= 99; x++) {
+//     arrayOfShips1.push([x]);
+//     let id = x.toString();
+//     positionCounter = {
+//       ...positionCounter,
+//       [id]: 0,
+//     };
+//   }
+//   allPositions = [
+//     ...arrayOfShips1,
+//     ...arrayOfShips2,
+//     ...arrayOfShips3,
+//     ...arrayOfShips4,
+//   ];
+// }
+// allShipsCombine();
+// let arrOfChance = [];
+// calculateSquareProbability(allPositions);
+// possibilityArray();
+// let shotSelected = randomSelectFromArr(arrOfChance);
+// let injuredTrigger = false;
+// let positionsWhenHit = [];
+// let injuredPositions = [];
+// pcLogic(shotSelected);
 
-allShipsCombine();
+// function calculateSquareProbability(arr) {
+//   arr.map((item) => {
+//     item.map((el) => {
+//       let id = el.toString();
+//       positionCounter[id]++;
+//     });
+//   });
+// }
 
-function calculateSquareProbability() {
-  allPositions.map((item) => {
-    item.map((el) => {
-      let id = el.toString();
-      positionCounter[id]++;
-    });
-  });
-}
-calculateSquareProbability();
+// function possibilityArray() {
+//   for (let option in positionCounter) {
+//     for (let x = 0; x < positionCounter[option]; x++) {
+//       arrOfChance.push(option);
+//     }
+//   }
+// }
 
-let entries = Object.entries(positionCounter);
+// function makeValues0(obj) {
+//   for (let position in obj) {
+//     obj[position] = 0;
+//   }
+//   return obj;
+// }
 
-// console.log(allPositions);
-// console.log(positionCounter);
-// console.log(entries);
+// function pcLogic(shotSelected) {
+//   let interval = setInterval(() => {
+//     color(shotSelected, selectedShips);
+//     let array = Array.from(container.children);
+//     if (array[shotSelected].classList.contains("missed")) {
+//       allPositions = allPositions.filter(
+//         (item) => !item.includes(Number(shotSelected))
+//       );
+//       positionCounter = makeValues0(positionCounter);
+//       console.log(positionCounter);
+//       calculateSquareProbability(allPositions);
+//       console.log(positionCounter);
+//       arrOfChance = [];
+//       possibilityArray();
+//       // console.log(arrOfChance);
 
-let arrOfChance = [];
+//       shotSelected = randomSelectFromArr(arrOfChance);
+//     } else if (array[shotSelected].classList.contains("injured")) {
+//       pcLogicWhenInjured();
+//     }
+//   }, 5000);
 
-function possibilityArray() {
-  for (let option in positionCounter) {
-    for (let x = 0; x < positionCounter[option]; x++) {     
-      arrOfChance.push(option);
-    }
-  }
-}
-possibilityArray()
+//   // interval()
+// }
 
-console.log(arrOfChance);
+// function pcLogicWhenInjured() {
+//   injuredPositions.push(shotSelected);
+//   console.log(injuredPositions);
+//   if (injuredTrigger) {
+//     positionsWhenHit = positionsWhenHit.filter((item) =>
+//       item.includes(Number(shotSelected))
+//     );
+//   } else {
+//     positionsWhenHit = allPositions.filter((item) =>
+//       item.includes(Number(shotSelected))
+//     );
+//   }
+//   positionCounter = makeValues0(positionCounter);
+//   calculateSquareProbability(positionsWhenHit);
+//   injuredPositions.map((item) => {
+//     positionCounter[item] = 0;
+//   });
+//   arrOfChance = [];
+//   arrOfChance = sortByProbability(positionCounter);
+//   possibilityArray();
+//   console.log(arrOfChance);
+//   shotSelected = arrOfChance[1][0];
+//   injuredTrigger = true;
+// }
+
+// function sortByProbability(obj) {
+//   let arr = [];
+//   for (let number in obj) {
+//     arr.push([number, obj[number]]);
+//   }
+//   arr.sort((a, b) => {
+//     return b[1] - a[1];
+//   });
+//   return arr;
+// }
